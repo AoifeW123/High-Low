76c51af382401ef04c2190cf1affc70c643e8c0f
AoifeW123 <43203143+AoifeW123@users.noreply.github.com>
2018-11-04 21:43:40 +0000

    Create FirstAttempt.cpp

---
+++ FirstAttempt.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+
+{
+
+	int GeneratedNum, PlayerGuess;
+
+	srand(time(0)); //seed random number generator
+
+  // THis will be a numbe chosen randomly by the computer 
+	GeneratedNum = rand() % 10 + 1; // random number between 1 and 10
+  
+  //This will be a number provided by the user, from an input.
+  //Needs to change from a user input to a another generated number
+
+  int PlayersGuess = rand() % 10 + 1;
+
+	cout << "Welcome to Higher or Lower! The game  Where you have to Guess if you number will be higher or lower!\n\n";
+
+	do
+
+	{
+
+		cout << "Enter a guess between 1 and 10 : ";
+
+		cin >> PlayersGuess;
+
+		;
+
+		if (PlayersGuess > GeneratedNum)
+
+			cout << "Too high!\n\n";
+
+		else if (PlayersGuess < GeneratedNum)
+
+			cout << "Too low!\n\n";
+
+		
+    else if (PlayersGuess == GeneratedNum)
+
+      cout<< "Draw!\n";
+    
+  
+
+	} while (PlayersGuess != GeneratedNum);
+
+
+
+	cin.ignore();
+
+	cin.get();
+
+	return 0;
+
+}
